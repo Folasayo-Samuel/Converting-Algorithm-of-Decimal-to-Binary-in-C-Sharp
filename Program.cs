6c5fcb7eaccf7e96b5bb158403ca6f076f227910
Folasayo Samuel <folasayosamuel@gmail.com>
2022-10-17 13:14:26 +0100

    update

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConvertAlgoDecToBi;
+
+public class Program
+{
+	static void Main(string[]args)
+	{
+		Console.Write("Enter a number: ");
+		int number = Convert.ToInt32(Console.ReadLine());
+		string binary = Convert.ToString(number, 2);
+		Console.WriteLine($"The Binary format for {number} is {binary}");
+
+        Console.ReadLine();
+    }
+}
